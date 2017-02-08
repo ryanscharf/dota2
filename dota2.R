46e94479d2f01a0fb990bb6406c024c39ffa2f4d
ryanscharf <ryan.scharf@gmail.com>
2017-02-08 16:06:50 -0500

    updated dota2.r

--- dota2.R
+++ dota2.R
@@ -1,9 +1,6 @@
 #keep relevant information from matches
 matches_i <- match[, c(1,10)]
 
-#copy match outcome to players_hero
-playershero$radiant_win <- matches_i[match(playershero$match_id, matches_i$match_id), 2]
-
 #let's get rid of some missing data
 badheroids <- players[players$hero_id == 0, ]
 playershero <- players[!players$match_id %in% badheroids$match_id,]
@@ -11,4 +8,52 @@ playershero <- players[!players$match_id %in% badheroids$match_id,]
 #only keep hero information from players
 playershero <- playershero[, c(1,3,4)]
 
-#
+#copy match outcome to players_hero
+playershero$radiant_win <- matches_i[match(playershero$match_id, matches_i$match_id), 2]
+
+#reshape the data from long to wide format with tidyr
+library("tidyr")
+playershero <- spread(playershero, player_slot, hero_id)
+
+#create results matrix for radiant and dire
+radiantwinrate <- matrix(data = NA, nrow = 112, ncol = 112)
+colnames(radiantwinrate) <- c(hero_names$hero_id)
+rownames(radiantwinrate) <- c(hero_names$hero_id)
+direwinrate <- matrix(data = NA, nrow= 112, ncol = 112)
+colnames(direwinrate) <- c(hero_names$hero_id)
+rownames(direwinrate) <- c(hero_names$hero_id)
+
+#create subsets for radiant wins/losses, and dire wins/losses
+radiantwin <- filter(playershero, radiant_win == "True")
+radiantloss <- filter(playershero, radiant_win == "False")
+direwin <- filter(playershero, radiant_win == "False")
+direloss <- filter(playershero, radiant_win == "True")
+
+#create hero pair permutations for radiant and dire of each match
+test<- combn(Ttest[,3:7], 2, simplify=FALSE)
+#df <- data.frame(matrix(unlist(test),ncol = 2, byrow= T))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#lookup table for hero names
