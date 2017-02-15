27bd5dd24681dfdbfa53945408e66b1786a7c743
ryanscharf <ryan.scharf@gmail.com>
2017-02-15 15:03:49 -0500

    old pairing method didn't work; added new one

--- dota2.R
+++ dota2.R
@@ -33,8 +33,16 @@ radiantloss <- filter(playershero, radiant_win == "False")
 direwin <- filter(playershero, radiant_win == "False")
 direloss <- filter(playershero, radiant_win == "True")
 
+#get rid of irrelevant team for each category
+radiantwin[,8:12]<- NULL
+radiantloss[,8:12] <- NULL
+direwin[,3:7] <- NULL
+direloss[,3:7] <- NULL
+
+######
 #create hero pair permutations for radiant and dire of each match
-test<- apply(Ttest[,3:7], 1, function(x) combn(x, 2, simplify = F))
+ptm <- proc.time()
+test<- apply(radiantloss[,3:7], 1, function(x) combn(x, 2, simplify = F))
 df <- data.frame(matrix(unlist(test),ncol = 2, byrow= T))
 
 #add back the match_id and radiant_win and clean up columns
@@ -44,51 +52,49 @@ df$radiant_win <- NA
 df <- df[,c(3,4,1,2)]
 
 #add back in match_ids
-for (i in 1:length(Ttest$match_id)){
+for (i in 1:length(radiantloss$match_id)){
   x = i * 10 -9
-df[x:(x+9),1] <- paste(Ttest[i,1]);
+df[x:(x+9),1] <- paste(radiantloss[i,1]);
 }
 
 #add back in radiant_win
-for (i in 1:length(Ttest$radiant_win)){
+for (i in 1:length(radiantloss$radiant_win)){
   x = i * 10 -9
-  df[x:(x+9),2] <- paste(Ttest[i,2]);
+  df[x:(x+9),2] <- paste(radiantloss[i,2]);
 }
 
-#get combination frequencies
-ptm <- proc.time()
+#get combination frequencies through column flipping and comparison
+#####DOESN'T WORK#########
 
-comb1 <- df
-comb2 <- df[,c(1,2,4,3)]
-colnames(comb2) <- c("match_id", "radiant_win", "Hero1", "Hero2")
-combcount <- rbind(comb1, comb2)
-combcount <- plyr::count(combcount, vars = c("Hero1", "Hero2"))
+#ptm <- proc.time()
+
+#comb1 <- df
+#comb2 <- df[,c(1,2,4,3)]
+#colnames(comb2) <- c("match_id", "radiant_win", "Hero1", "Hero2")
+#combcount <- rbind(comb1, comb2)
+#combcount <- plyr::count(combcount, vars = c("Hero1", "Hero2"))
 #add identifiers to each column for using the %in% function  later
-comb1$colid <- paste(comb1$Hero1, comb1$Hero2, sep=",")
-combcount$colid <- paste(combcount$Hero1, combcount$Hero2, sep=",")
-combcount <- combcount[combcount$colid %in% comb1$colid,]
-combcount$colid <- NULL
+#comb1$colid <- paste(comb1$Hero1, comb1$Hero2, sep=",")
+#combcount$colid <- paste(combcount$Hero1, combcount$Hero2, sep=",")
+#combcount <- combcount[combcount$colid %in% comb1$colid,]
+#combcount$colid <- NULL
 
-proc.time() - ptm
+#proc.time() - ptm
 
 
 #apply version of combination frequencies
-#ptm <- proc.time()
 
-#df$Hero1 <- as.numeric(as.character(df$Hero1))
-#df$Hero2 <- as.numeric(as.character(df$Hero2))
-#df$min <- apply(df[,3:4],1,min)
-#df$max <- apply(df[,3:4],1,max)
-#df$Hero1 <- NULL
-#df$Hero2 <- NULL
-#colnames(df) <- c("match_id", "radiant_win", "Hero1", "Hero2")
-#combcount <- plyr::count(df, vars = c("Hero1", "Hero2"))
-##add identifiers to each column for using the %in% function  later
-##comb1$colid <- paste(comb1$Hero1, comb1$Hero2, sep=",")
-##combcount$colid <- paste(combcount$Hero1, combcount$Hero2, sep=",")
-##combcount <- combcount[combcount$colid %in% comb1$colid,]
-##combcount$colid <- NULL
 
-#proc.time() - ptm
+df$Hero1 <- as.numeric(as.character(df$Hero1))
+df$Hero2 <- as.numeric(as.character(df$Hero2))
+df$min <- apply(df[,3:4],1,min)
+df$max <- apply(df[,3:4],1,max)
+df$Hero1 <- NULL
+df$Hero2 <- NULL
+colnames(df) <- c("match_id", "radiant_win", "Hero1", "Hero2")
+combcount <- plyr::count(df, vars = c("Hero1", "Hero2"))
+
+
+proc.time() - ptm
 
 #lookup table for hero names
