99fbdef2fc07cb0e7b2c48c8098de3ca0c6abafe
ryanscharf <ryan.scharf@gmail.com>
2017-02-27 21:45:02 -0500

    computed all counts, created first version of visualization of wins

--- dota2.R
+++ dota2.R
@@ -42,8 +42,8 @@ direloss[,3:7] <- NULL
 
 ######
 #create hero pair permutations for radiant and dire of each match
-ptm <- proc.time()
-test<- apply(radiantloss[,3:7], 1, function(x) combn(x, 2, simplify = F))
+#ptm <- proc.time()
+test<- apply(direloss[,3:7], 1, function(x) combn(x, 2, simplify = F))
 df <- data.frame(matrix(unlist(test),ncol = 2, byrow= T))
 
 #add back the match_id and radiant_win and clean up columns
@@ -53,21 +53,21 @@ df$radiant_win <- NA
 df <- df[,c(3,4,1,2)]
 
 #add back in match_ids
-for (i in 1:length(radiantloss$match_id)){
+for (i in 1:length(direloss$match_id)){
   x = i * 10 -9
-df[x:(x+9),1] <- paste(radiantloss[i,1]);
+df[x:(x+9),1] <- paste(direloss[i,1]);
 }
 
 #add back in radiant_win
-for (i in 1:length(radiantloss$radiant_win)){
+for (i in 1:length(direloss$radiant_win)){
   x = i * 10 -9
-  df[x:(x+9),2] <- paste(radiantloss[i,2]);
+  df[x:(x+9),2] <- paste(direloss[i,2]);
 }
+
+direlossvals <- df
 
 #get combination frequencies through column flipping and comparison
 #####DOESN'T WORK#########
-
-#ptm <- proc.time()
 
 #comb1 <- df
 #comb2 <- df[,c(1,2,4,3)]
@@ -80,22 +80,107 @@ for (i in 1:length(radiantloss$radiant_win)){
 #combcount <- combcount[combcount$colid %in% comb1$colid,]
 #combcount$colid <- NULL
 
-#proc.time() - ptm
 
 
 #apply version of combination frequencies
 
+countperms <- function(xz){
+xz$Hero1 <- as.numeric(as.character(xz$Hero1))
+xz$Hero2 <- as.numeric(as.character(xz$Hero2))
+xz$min <- apply(xz[,3:4],1,min)
+xz$max <- apply(xz[,3:4],1,max)
+xz$Hero1 <- NULL
+xz$Hero2 <- NULL
+colnames(xz) <- c("match_id", "radiant_win", "Hero1", "Hero2")
+combcount <- plyr::count(xz, vars = c("Hero1", "Hero2"))
 
-df$Hero1 <- as.numeric(as.character(df$Hero1))
-df$Hero2 <- as.numeric(as.character(df$Hero2))
-df$min <- apply(df[,3:4],1,min)
-df$max <- apply(df[,3:4],1,max)
-df$Hero1 <- NULL
-df$Hero2 <- NULL
-colnames(df) <- c("match_id", "radiant_win", "Hero1", "Hero2")
-combcount <- plyr::count(df, vars = c("Hero1", "Hero2"))
+assign(paste("count", deparse(substitute(xz)) , sep="_"), xz, envir=.GlobalEnv)
+}
+
+wtf <- function(xz){
+xz <- plyr::count(xz$x1)
+assign(paste("count", deparse(substitute(xz)) , sep=""), xz, envir=.GlobalEnv)
+}
+
+myfunc <- function(v1) {
+  deparse(substitute(v1))
+}
+
+######long dumb version of permutation frequencies
+###############################################################
+rwcounts <- radiantwinvals
+  rwcounts$Hero1 <- as.numeric(as.character(rwcounts$Hero1))
+  rwcounts$Hero2 <- as.numeric(as.character(rwcounts$Hero2))
+  rwcounts$min <- apply(rwcounts[,3:4],1,min)
+  rwcounts$max <- apply(rwcounts[,3:4],1,max)
+  rwcounts$Hero1 <- NULL
+  rwcounts$Hero2 <- NULL
+  colnames(rwcounts) <- c("match_id", "radiant_win", "Hero1", "Hero2")
+  rwcounts <- plyr::count(rwcounts, vars = c("Hero1", "Hero2"))
+  rwcounts <- arrange(rwcounts, desc(freq))
+  
+rlcounts <- radiantlossvals
+  rlcounts$Hero1 <- as.numeric(as.character(rlcounts$Hero1))
+  rlcounts$Hero2 <- as.numeric(as.character(rlcounts$Hero2))
+  rlcounts$min <- apply(rlcounts[,3:4],1,min)
+  rlcounts$max <- apply(rlcounts[,3:4],1,max)
+  rlcounts$Hero1 <- NULL
+  rlcounts$Hero2 <- NULL
+  colnames(rlcounts) <- c("match_id", "radiant_win", "Hero1", "Hero2")
+  rlcounts <- plyr::count(rlcounts, vars = c("Hero1", "Hero2"))
+  rlcounts <- arrange(rlcounts, desc(freq))
+  
+dwcounts <- direwinvals
+  dwcounts$Hero1 <- as.numeric(as.character(dwcounts$Hero1))
+  dwcounts$Hero2 <- as.numeric(as.character(dwcounts$Hero2))
+  dwcounts$min <- apply(dwcounts[,3:4],1,min)
+  dwcounts$max <- apply(dwcounts[,3:4],1,max)
+  dwcounts$Hero1 <- NULL
+  dwcounts$Hero2 <- NULL
+  colnames(dwcounts) <- c("match_id", "radiant_win", "Hero1", "Hero2")
+  dwcounts <- plyr::count(dwcounts, vars = c("Hero1", "Hero2"))
+  dwcounts <- arrange(dwcounts, desc(freq))
+  
+dlcounts <- direlossvals
+  dlcounts$Hero1 <- as.numeric(as.character(dlcounts$Hero1))
+  dlcounts$Hero2 <- as.numeric(as.character(dlcounts$Hero2))
+  dlcounts$min <- apply(dlcounts[,3:4],1,min)
+  dlcounts$max <- apply(dlcounts[,3:4],1,max)
+  dlcounts$Hero1 <- NULL
+  dlcounts$Hero2 <- NULL
+  colnames(dlcounts) <- c("match_id", "radiant_win", "Hero1", "Hero2")
+  dlcounts <- plyr::count(dlcounts, vars = c("Hero1", "Hero2"))
+  dlcounts <- arrange(dlcounts, desc(freq))  
+  
+  
+
+library("reshape2")
+library("viridis")
+library("scales")
+library("ggthemes")
+   radiant_pairing_winm <- reshape2::acast(rwcounts, Hero1~Hero2, value.var = "freq", drop = FALSE)
+  
 
 
-proc.time() - ptm
+   
+   
+   
+   ##plottin
+gg <- ggplot(rwcounts, aes(x=Hero1, y=Hero2, fill=freq)) + geom_tile(color="white", size=0.4) + 
+  scale_fill_viridis(name="# of Wins", label=comma) + coord_equal() + theme_tufte(base_family="Helvetica") +
+  scale_x_continuous(breaks = unique(rwcounts$Hero1), position = "top")  + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+  scale_y_continuous(trans = "reverse", breaks = unique(rwcounts$Hero2), position = "top") +
+  theme(plot.background = element_rect(fill = 'grey'), panel.grid.major = element_line(color= "black"))
 
+
+
+gg
+
+#ptm <- proc.time()
+#proc.time() - ptm
+
+#radiant win%
+  21922/(20298+21922)
+  
 #lookup table for hero names
